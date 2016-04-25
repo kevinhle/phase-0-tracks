276ac8f0dbe43f9d85352b57adcb3ad2a6839e8f
Kevin Le <kevin.whoo.le@gmail.com>
2016-04-25 11:42:45 -0700

    5.1 method detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,48 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
+result = 'iNvEsTiGaTiOn'.swapcase
+p result
 # => “InVeStIgAtIoN”
 
-# “zom”.<???>
+result1 = 'zoom'.insert(2, 'o')
+p result1
 # => “zoom”
 
-# “enhance”.<???>
+result2 = 'enhance'.center(15)
+p result2
 #=> “    enhance    ”
 
-# “Stop! You’re under arrest!”.<???>
+result3 = 'Stop! You’re under arrest!'.upcase
+p result3
 # => “STOP! YOU’RE UNDER ARREST!”
 
-# “the usual”.<???>
+result2 = 'the usual' << ' suspects'
+p result4
 #=> “the usual suspects”
 
-#“ suspects”.<???>
+s = ' suspects'
+s.insert(0, 'the usual')
 #=> “the usual suspects”
 
-# “The case of the disappearing last letter”.<???>
+result5 = 'The case of the disappearing last letter'.chop
+p result5
 # => “The case of the disappearing last lette”
 
-# “The mystery of the missing first letter”.<???>
+result6 = 'The mystery of the missing first letter'[1..-1]
+p result6
 # => “he mystery of the missing first letter”
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
+result7 = 'Elementary  ,    my   dear      Watson!'.squeeze(' ')
+p result7
 # => “Elementary, my dear Watson!”
 
-#“z”.<???>
+result8 = 'z'.ord
+p result8
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# in ASCII code 122 is the numerical representation of the character "z"
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+result9 = 'How many times does the letter ‘a’ appear in this string?'.count"a"
+p result9
 #=> 4
