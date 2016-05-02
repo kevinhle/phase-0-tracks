d56d7588e3a6bf1ee8d6b891e9b51c11e2f144b9
Kevin Le <kevin.whoo.le@gmail.com>
2016-05-02 14:48:59 -0700

    Puppy class design

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# SPECIES ------------------------
+# Puppyland  Adorable
+#
+# CHARACTERISTICS ----------------
+# Fur length: short
+# Fur pattern: curly
+# Friendly: yes
+# Eye count: 2
+# Name: varies
+# Color: varies
+
+#
+# BEHAVIOR -----------------------
+# Jumps
+# Runs
+# Fetch
+# Plays dead
+# Barks on commandcdadad
